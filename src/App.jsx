31a5a950ac987a10978df3abf247a736296dd83c
“Cosimo <pastore_cosimo@yahoo.it>
2025-02-03 18:09:33 +0100

    Add Image

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import ButtonComponent from "./components/ButtonComponent";
+import ImageComponent from "./components/imgComp";
 
 function App() {
   return (
     <div className="main-container">
       <ButtonComponent text="Aggiungi" />
+      <ImageComponent />
     </div>
   );
 }
--- src/components/imgComp.jsx
+++ src/components/imgComp.jsx
@@ -1,1 +1,16 @@
-class ImageComponent
+import { Component } from "react";
+
+class ImageComponent extends Component {
+  render() {
+    console.log("THIS", this);
+
+    return (
+      <img
+        src="https://images.unsplash.com/photo-1735596365888-ad6d151533f2?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="Klausenplas"
+        width={200}
+      />
+    );
+  }
+}
+export default ImageComponent;
